566b7f7f1d1fb3c202bf85ef541348fb1691d5a1
excellen114514 <xiuxianing@foxmail.com>
2024-09-30 21:07:09 +0800

    优化一下玩家数据更新

--- apps/DataUpdate.js
+++ apps/DataUpdate.js
@@ -1,5 +1,9 @@
-import fs from 'fs/promises';
-import path from 'path';
+
+import {
+  getPlayerData,
+  isPlayerExist,
+  writeGameData
+} from '../function.js';
 
 const Gupdate =/^(#|\/)?玩家数据更新$/
 export class Kc extends plugin {
@@ -20,43 +24,37 @@ export class Kc extends plugin {
 
     async gupdate(e){
       if (e.isGroup) 
-        return e.reply(['覆盖数据是危险的，仅能在私聊覆盖'])
-        const newEntry = {
-            mlv: "",
-            lv: 0,
-            qy: 0,
-            cd: { knock: 0, random: 0,money: 0, break:0},
-            log: { knock: [], random: [], money: [], break:[]}
-        };  
-        const ID = [e.user_id]
-        const currentWorkingDirectory = process.cwd();
-          // 定义保存文件的路径
-        const filePath = path.join(currentWorkingDirectory, 'plugins' , 'KnockMuYu-plugin', 'data', 'player', ID + '.json');
-    
-        fs.readFile(filePath)
-        .then((data) => {
-          try {
-            // 解析现有 JSON 数据为一个对象
-            let jsonData = JSON.parse(data);
-      
-            // 添加新的键值对
-            jsonData = { ...jsonData, ...newEntry };
-      
-            // 将修改后的对象转换回 JSON 字符串
-            const updatedJsonString = JSON.stringify(jsonData, null, 2);
-      
-            // 将更新后的 JSON 字符串写回文件
-            return fs.writeFile(filePath, updatedJsonString);
-          } catch (err) {
-            // 如果解析 JSON 时发生错误，打印错误信息
-            console.error('Error parsing JSON data:', err);
-          }
-        })
-        .then(() => console.log('JSON data updated and saved to', filePath))
-        .catch((err) => {
-          // 如果写入文件时发生错误，打印错误信息
-          console.error('Error writing file:', err);
-        });
-        return e.reply('数据覆盖完成')
+      return e.reply(['覆盖数据是危险的，仅能在私聊覆盖'])
+      if (!isPlayerExist(ID[0])) {
+        e.reply("你还没有木鱼哦~发送 查看木鱼 来获得你的第一个木鱼吧！")
+      }
+      const ID = [e.user_id]
+      const USER_DATA = await getPlayerData(ID[0]);
+      if(USER_DATA.hasOwnProperty('lv')){
+        
+        e.reply(`lv已经存在,开始写入下一个键值`)
+       }else {
+        const newEntry = {lv: 0};
+        writeGameData(ID, newEntry);
+        e.reply(`lv写入完成`)
+       }
+      if(USER_DATA.hasOwnProperty('mlv')){
+        e.reply(`mlv已经存在，开始写入下一个键值`)
+      }else {
+        const newEntry ={mlv: "凡人"};
+        writeGameData(ID, newEntry)
+        e.reply(`mlv写入完成`)
+      }
+      if(USER_DATA.hasOwnProperty('qy')){
+        e.reply(`qy已经存在，开始写入下一个键值`)
+      }else {
+        const newEntry ={qy: 0};
+        writeGameData(ID, newEntry)
+        e.reply(`qy写入完成`)
+      }
+      const newEntry ={ cd: { knock: 0, random: 0,money: 0, break:0},
+      log: { knock: [], random: [], money: [], break:[]}}
+      writeGameData(ID, newEntry)
+      return e.reply(`完成，重新刷新冷却与日志`)
     }
 }
--- apps/Muyu.js
+++ apps/Muyu.js
@@ -47,7 +47,7 @@ export class gmy extends plugin {
          /** 用户数据 */
          const USER_DATA = await getPlayerData(ID[0])
 
-         msg = `你现在的功德为${USER_DATA['number']}，有${USER_DATA['money']}银两，等级为${USER_DATA['lv']}!境界为${USER_DATA['mlv']}`
+         msg = `你现在的功德为${USER_DATA['number']}，\n有${USER_DATA['money']}银两，\n等级为${USER_DATA['lv']}!\n境界为${USER_DATA['mlv']}`
 
          return e.reply([msg], true)
      } else { // 看别人
@@ -57,7 +57,7 @@ export class gmy extends plugin {
          /** 用户数据 */
          const USER_DATA = await getPlayerData(ID[1])
 
-         msg = `@${USER_DATA['name']}现在的功德为${USER_DATA['number']}，有${USER_DATA['money']}银两，等级为${USER_DATA['lv']}!境界为${USER_DATA['mlv']}`
+         msg = `@${USER_DATA['name']}现在的功德为${USER_DATA['number']}，\n有${USER_DATA['money']}银两，\n等级为${USER_DATA['lv']}!\n境界为${USER_DATA['mlv']}`
          return e.reply([msg], true)
      }
     } 
