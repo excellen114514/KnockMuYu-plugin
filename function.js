1d24536c8f10da7bbe939408110e368eeb32277c
excellen114514 <xiuxianing@foxmail.com>
2024-09-30 21:07:41 +0800

    添加一个新模块

--- function.js
+++ function.js
@@ -1,6 +1,7 @@
 /**这里的模块导出借鉴面包文游,仅是在其基础移除了创立群文件夹群使其可以跨群 */
 
 import fs from 'fs'; 
+import fsp from 'fs/promises'
 import path from 'path'; 
 import Yaml from 'yaml';
 
@@ -53,3 +54,23 @@ export function getCurrentDate() {
     return new Date().toLocaleString('zh-CN', { timeZone: 'Asia/Shanghai' })
 }
 
+export async function writeGameData(ID, newEntry){
+    const currentWorkingDirectory = process.cwd();
+          // 定义保存文件的路径
+    const filePath = path.join(currentWorkingDirectory, 'plugins' , 'KnockMuYu-plugin', 'data', 'player', ID + '.json');
+    
+    const data = await fsp.readFile(filePath)
+            // 解析现有 JSON 数据为一个对象
+    let jsonData = JSON.parse(data);
+      
+            // 添加新的键值对
+    jsonData = { ...jsonData, ...newEntry };
+      
+            // 将修改后的对象转换回 JSON 字符串
+    const updatedJsonString = JSON.stringify(jsonData, null, 2);
+      
+            // 将更新后的 JSON 字符串写回文件
+    return fsp.writeFile(filePath, updatedJsonString);
+           
+        
+}
