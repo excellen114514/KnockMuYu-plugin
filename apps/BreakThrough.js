47059f557df3e20dd718f2e64e816cd4b9093027
excellen114514 <xiuxianing@foxmail.com>
2024-10-26 21:48:49 +0800

    更新 BreakThrough.js

--- apps/BreakThrough.js
+++ apps/BreakThrough.js
@@ -158,7 +158,7 @@ export class gmy extends plugin {
   
 }
    async mbreak(e){
-     if (e.isGroup) 
+     if (!e.isGroup) 
       return e.reply(['木鱼只能在群聊敲哦~'])
       /** 配置数据 */
       const configData = await readConfiguration()
